42f51c96b5d7539a9da7f010e773ba1f89f23f60
td-diana <99271477+td-diana@users.noreply.github.com>
2022-10-10 19:58:59 +0300

    cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
@@ -8,6 +9,8 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(cors());
+
 app.use(bodyParser.json());
 
 app.use(requestLogger); // подключаем логгер запросов
